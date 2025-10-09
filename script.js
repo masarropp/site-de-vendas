3036af155b6bc350a1d756ad02f1bcb189f27f2d
masarropp <gabriel.daniel.correa@escola.pr.gov.br>
2025-10-09 11:54:22 -0300

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("form-contato").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Sua mensagem foi enviada com sucesso! Entraremos em contato em breve.");
+});
